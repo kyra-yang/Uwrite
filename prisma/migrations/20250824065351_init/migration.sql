7ee8d524e92ea682babbeb9d45b3e57bfd928ce9
Kyra <hsir48753@gmail.com>
2025-08-24 16:55:03 +1000

    initialise postgresql

---
+++ prisma/migrations/20250824065351_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "public"."Project" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
